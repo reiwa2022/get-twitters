8f3fe4cc6a39612cc48f7004833a43b62acdacd8
reiwa2021 <tetuya@qd5.so-net.ne.jp>
2023-04-14 15:40:14 +0900

    カルーセル高さ対応

--- public/js/carousel-height-reset.js
+++ public/js/carousel-height-reset.js
@@ -3,17 +3,23 @@
 // 説明: カルーセルの高さをスマホ/PCの切り替え時にリセットします。
 //
 
+// メディアクエリ文字列でMediaQueryListオブジェクトを取得します。
 const mql = window.matchMedia('(min-width:768px)');
+// カルーセルのインナー要素ノードを取得します。
 const inner = document.getElementsByClassName('carousel-inner').item(0);
-
-mql.addEventListener('change', (e) => {
-  if (e.matches) {
-    if (inner.style.height) {
-      inner.style.height = null;
+// カルーセルのインナー要素ノードが存在した場合、メディアクエリでの変化イベントを待機します。
+if (inner) {
+  mql.addEventListener('change', (e) => {
+    // 画面幅がメディアクエリより大きくなった場合、カルーセルインナー要素の高さをリセットします。
+    if (e.matches) {
+      if (inner.style.height) {
+        inner.style.height = null;
+      }
+      // 画面幅がメディアクエリより小さくなった場合、カルーセルインナー要素の高さをリセットします。
+    } else {
+      if (inner.style.height) {
+        inner.style.height = null;
+      }
     }
-  } else {
-    if (inner.style.height) {
-      inner.style.height = null;
-    }
-  }
-});
+  });
+}
